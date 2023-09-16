c94a389cc2bee41eb8852e5f294e0c9222f56fa8
fatoni-ach <fatoni.noah@gmail.com>
2023-09-16 11:46:06 +0700

    simple hello world app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fatoni-ach/app-sayhello
+
+go 1.18
+
+require github.com/fatoni-ach/go-module-sayhello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	sayhello "github.com/fatoni-ach/go-module-sayhello/v2"
+)
+
+func main() {
+	fmt.Println(sayhello.Hello("Anonim"))
+}
